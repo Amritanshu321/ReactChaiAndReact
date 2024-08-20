52aa456b17d4bdae14af24543d98ce7d14f1ee79
Amritanshu Mishra <amrianshum786@gmail.com>
2024-08-21 00:17:38 +0530

    understanding file structure

---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h3>Hello From chai</h3>
+    )
+}
+export default Chai
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,13 @@
-
+import Chai from './chai.jsx'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
+   <>
     <App />
-  </StrictMode>,
+    <Chai />
+   </>
+  
 )
