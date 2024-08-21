7cfb4ece46d8f9d1c34289b839aeff5bd0e0f59b
Amritanshu Mishra <amrianshum786@gmail.com>
2024-08-21 23:50:53 +0530

    understanding the working of react

---
+++ customReact/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./Chai"
+function App() {
+  const username="chai and React"
+
+  return (
+    <>
+      <h2>Hello from App {username}</h2>
+      <Chai/>
+    </>
+  )
+}
+
+export default App
