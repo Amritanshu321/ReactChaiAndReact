5b6e63ac212cdd0467f63607c917a65aa43ccc27
Amritanshu Mishra <makazamritanshu@gmail.com>
2025-04-05 18:59:27 +0530

    till blog auth

---
+++ 12megablog/src/store/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit'
+import { login, logout } from './authSlice'
+const store= configureStore({
+    reducer:{
+        login:login,
+        logout:logout
+    }
+})
+
+export default store
